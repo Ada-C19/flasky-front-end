6f93b1d90aea26ddc4ecda170e81a52f348c0ff0
Kaliane <vankaliane@gmail.com>
2023-06-12 11:40:58 -0400

    Created CrstyalList and Crystal components.

---
+++ src/components/Crystal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Crystal = () => {
+    return (
+        <li>
+            <h2>Crystal List</h2>
+            <p>Crystal Color</p>
+            <p>Crystal Powers</p>
+            <button>Charge Crystal</button>
+        </li>
+    );
+};
+
+export default Crystal

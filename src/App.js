e2c7a7e27c43d33e12fbb0ed5a5c0447de60605c
Ansel Rognlie <ansel.rognlie@gmail.com>
2023-06-13 11:58:38 -0700

    Roundtable: Props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import CatList from './components/CatList';
 
 import './App.css';
 
+const DATA = [
+  {
+    name: "Ubik",
+    caretaker: "Maria",
+    color: "grey",
+    personality: "wild child",
+  },
+  {
+    name: "Pepper",
+    caretaker: "Mark",
+    color: "black",
+    personality: "spicy",
+  },
+  {
+    name: "Binx",
+    caretaker: "Susan",
+    color: "tuxedo",
+    personality: "feral",
+  },
+];
+
+
 function App() {
   return (
     <main className="App">
       <h1>The Cat Corner</h1>
-      <CatList />
+      <CatList catData={DATA} />
     </main>
   );
 }
--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,16 +1,37 @@
 import React from "react";
 
 import Cat from "./Cat";
+import PropTypes from 'prop-types';
 
-const CatList = () => {
+const CatList = (props) => {
   return (
     <section>
-      <h2>Cat list</h2>
-      <ul>
-        <Cat />
+      <h2>Glaring size: { props.catData.length }</h2>
+      <ul> {
+        props.catData.map((cat, i) => (
+          <Cat 
+            name={cat.name}
+            personality={cat.personality}
+            caretaker={cat.caretaker}
+            color={cat.color}
+            key={i}
+          />
+        ))
+        }
       </ul>
     </section>
   );
+};
+
+CatList.propTypes = {
+  catData: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+      personality: PropTypes.string.isRequired,
+      caretaker: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default CatList;

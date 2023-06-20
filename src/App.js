91a628c829ca4f3ca653feda2d5311487c7b1784
claire <yue.katou@gmail.com>
2023-06-20 11:56:04 -0700

    installs axios. calls flasky backend to populate catData state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CatList from "./components/CatList";
-
+import axios from "axios";
 import "./App.css";
 
-const DATA = [
-  {
-    name: "Ubik",
-    caretaker: "Maria",
-    color: "grey",
-    personality: "wild child",
-    petCount: 6,
-    id: 1,
-  },
-  {
-    name: "Pepper",
-    caretaker: "Mark",
-    color: "black",
-    personality: "spicy",
-    petCount: 3,
-    id: 2,
-  },
-  {
-    name: "Binx",
-    caretaker: "Susan",
-    color: "tuxedo",
-    personality: "feral",
-    petCount: 8,
-    id: 3,
-  },
-];
-
+// const DATA = [
+//   {
+//     name: "Ubik",
+//     caretaker: "Maria",
+//     color: "grey",
+//     personality: "wild child",
+//     petCount: 6,
+//     id: 1,
+//   },
+//   {
+//     name: "Pepper",
+//     caretaker: "Mark",
+//     color: "black",
+//     personality: "spicy",
+//     petCount: 3,
+//     id: 2,
+//   },
+//   {
+//     name: "Binx",
+//     caretaker: "Susan",
+//     color: "tuxedo",
+//     personality: "feral",
+//     petCount: 8,
+//     id: 3,
+//   },
+// ];
+const kBaseUrl = "http://127.0.0.1:5000/cats";
 function App() {
-  const [catData, setCatData] = useState(DATA);
+  const [catData, setCatData] = useState([]);
+
+  const catDataConvert = (res) => {
+    return res.map((cat) => {
+      return { ...cat, petCount: cat.pet_count, caretaker: "" };
+    });
+  };
+
+  useEffect(() => {
+    axios
+      .get(kBaseUrl)
+      .then((res) => setCatData(() => catDataConvert(res.data)))
+      .catch((err) => console.log(err));
+  }, []);
+
   const petCat = (id) => {
     setCatData((prev) => {
       return prev.map((cat) => {
@@ -45,10 +59,18 @@ function App() {
         }
       });
     });
+    axios
+      .patch(`${kBaseUrl}/${id}/pet`)
+      .then((res) => console.log("Back end response", res.data))
+      .catch((err) => console.log(err));
   };
 
   const unregisterCat = (id) => {
     setCatData((prev) => prev.filter((cat) => cat.id !== id));
+    axios
+      .delete(`${kBaseUrl}/${id}`)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   };
 
   const totalPetTally = catData.reduce((total, cat) => {
@@ -57,7 +79,7 @@ function App() {
   }, 0);
 
   return (
-    <main className="App">
+    <main className='App'>
       <h1>The Cat Corner</h1>
       <h2>Total number of pets across all cats: {totalPetTally}</h2>
       <CatList

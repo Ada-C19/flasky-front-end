693d3147c860a598f7448c91f5339cd920ff265e
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-06-21 10:41:21 -0400

    Live Code from 6/21 - Add Create Crystal Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import CrystalList from './components/CrystalList';
+import CrystalForm from './components/CrystalForm';
 import axios from 'axios';
 
 const crystalData = [
@@ -9,71 +10,76 @@ const crystalData = [
     name: 'Amethyst',
     color: 'Purple',
     powers: 'Infinite knowledge and wisdom',
-    charges: 0
+    charges: 0,
   },
   {
     id: 2,
     name: "Tiger's Eye",
     color: 'Orange',
     powers: 'Confidence and strength',
-    charges: 0
+    charges: 0,
   },
   {
     id: 3,
     name: 'Rose Quarts',
     color: 'Pink',
     powers: 'Love',
-    charges: 0
+    charges: 0,
   },
 ];
 
 function App() {
-  const [crystals, setCrystals] = React.useState([])
+  const [crystals, setCrystals] = React.useState([]);
 
   React.useEffect(() => {
-    axios.get('http://localhost:5000/crystals').then(resp => {
-      setCrystals(resp.data)
-    })
-  },[])
+    axios.get('http://localhost:5000/crystals').then((resp) => {
+      setCrystals(resp.data);
+    });
+  }, []);
 
   const increaseCharge = (id) => {
-    axios.patch(`http://127.0.0.1:5000/crystals/${id}`).then(resp => {
-
-      setCrystals(prevCrystals => {
-        const updatedCrystals = prevCrystals.map(crystal => {
+    axios.patch(`http://127.0.0.1:5000/crystals/${id}`).then((resp) => {
+      setCrystals((prevCrystals) => {
+        const updatedCrystals = prevCrystals.map((crystal) => {
           // condition ? <value to return if true> : <value to return if false>
-          return crystal.id === id ? resp.data : crystal
-        })
-        return updatedCrystals
-      })
-    })
-    
-  }
+          return crystal.id === id ? resp.data : crystal;
+        });
+        return updatedCrystals;
+      });
+    });
+  };
 
   const removeCrystal = (id) => {
     axios.delete(`http://localhost:5000/crystals/${id}`).then(() => {
-      setCrystals(prevCrystals => {
-        const updatedCrystals = prevCrystals.filter(crystal => crystal.id !== id)
-        return updatedCrystals
-      })
-    })
-  }
+      setCrystals((prevCrystals) => {
+        const updatedCrystals = prevCrystals.filter(
+          (crystal) => crystal.id !== id
+        );
+        return updatedCrystals;
+      });
+    });
+  };
 
   const totalCharges = () => {
     let total = 0;
     for (let crystal of crystals) {
-        total += crystal.charges
+      total += crystal.charges;
     }
 
-    return total
-  }
+    return total;
+  };
 
   const title = 'The Crystal Cove';
   return (
     <main className='App'>
       <h1>{title}</h1>
       <p>Total Charges: {totalCharges()}</p>
-      <CrystalList crystals={crystals} removeCrystal={removeCrystal} increaseCharge={increaseCharge}/>
+      <CrystalForm />
+      <CrystalList
+        crystals={crystals}
+        removeCrystal={removeCrystal}
+        increaseCharge={increaseCharge}
+      />
     </main>
   );
 }
